13326520d0974f674c1cd71d27cf33cce916dd51
GerardR <GerardRegot@Gerards-MacBook-Pro.local>
2015-09-15 20:13:44 -0700

    added homepage

---
+++ homepage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN""http://www.w3.org/TR/html4/strict.dtd">
+<html>
+
+<head>
+	<link type="text/css" rel="stylesheet" href="stylesheet.css" />
+	<title>Biscuits | For Life</title>
+</head>
+
+<body>
+	<p>Hello</p>
+
+</body>
+</html>
